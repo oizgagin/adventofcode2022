f3ff86733cb71cfefca6dae478918fe9cbe8b9aa
Oleg Izgagin <37784140+oizgagin@users.noreply.github.com>
2022-12-09 23:05:49 +0600

    Add 9.2

---
+++ 09/2.py
@@ -0,0 +1,52 @@
+from collections import defaultdict
+
+
+def solution(dirs):
+    grid = defaultdict(lambda: defaultdict(int))
+
+    add = lambda t1, t2: (t1[0]+t2[0], t1[1]+t2[1])
+
+    dd = { 'U': (0, 1), 'D': (0, -1), 'L': (-1, 0), 'R': (1, 0) }
+
+    sign = lambda x: -1 if x < 0 else 1 if x > 0 else 0
+
+    def move(t, h):
+        if t[0] == h[0] and abs(t[1] - h[1]) == 2:
+            return (t[0], (t[1] + h[1])/2)
+        if t[1] == h[1] and abs(t[0] - h[0]) == 2:
+            return ((t[0] + h[0])/2, t[1])
+        if abs(t[0] - h[0]) == 2 or abs(t[1] - h[1]) == 2:
+            return (t[0] + sign(h[0]-t[0]), t[1] + sign(h[1]-t[1]))
+        return t
+
+    knots, prevknots = [(0, 0)] * 10, [(0, 0)] * 10
+
+    for d, c in dirs:
+        for _ in xrange(c):
+            knots[0] = add(knots[0], dd[d])
+
+            for i in xrange(1, 10):
+                knots[i] = move(prevknots[i], knots[i-1])
+
+            prevknots = knots
+
+            grid[knots[9][0]][knots[9][1]] += 1
+
+    res = 0
+    for i in grid.keys():
+        for j in grid[i].keys():
+            if grid[i][j] >= 1:
+                res += 1
+    return res
+
+
+def parse(input):
+    return map(lambda s: (s.split()[0], int(s.split()[1])), input.splitlines())
+
+
+def main():
+    input = open("input", "r").read()
+    print(solution(parse(input)))
+
+
+main()
